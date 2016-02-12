029c107acf0c897fe48132522d0f8b6db322901a
Alimov Tillo <alimovtillo@gmail.com>
2016-02-12 16:48:41 +0600

    add first laba

--- 1.py
+++ 1.py
@@ -1,1 +1,39 @@
-#bla bla
+# -*- coding: utf-8 -*-
+# Laba 1.1
+# x = int(raw_input("Введите число от 1 до 9:"))
+# if 1 <= x <= 3:
+#     print "1-3"
+#     s = (raw_input("Введите строку:"))
+#     n = int(raw_input("Введите число повторов строки:"))
+#     for i in range(n):
+#         print (s)
+# elif 3 < x <= 6:
+#     print "4-6"
+#     m = int(raw_input("Введите степень"))
+#     print(x ** m)
+# elif 6 < x <= 9:
+#     print "7-9"
+#     for i in range(10):
+#         x += 1
+#         print(x)
+# else:
+#     print "Ошибка ввода"
+#
+# Laba 1.2
+print("Общество в начале XXI века")
+x = int(raw_input("возраст:"))
+if 1 < x < 7:
+    print("Вам в детский сад")
+elif 7 < x < 18:
+    print("Вам в школу")
+elif 18 < x < 25:
+    print("Вам в профессиональное учебное заведение")
+elif 25 < x < 60:
+    print("Вам на работу")
+elif 60 < x <= 120:
+    print("Вам предоставляется выбор")
+else:
+    0 < x <= 121
+    for i in range(5):
+        print("Ошибка! Это программа для людей!")
+
